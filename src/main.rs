54758fba0994e524063686df02251cfc06377fda
may h <mehrabhoque@gmail.com>
2023-01-21 12:25:45 -0500

    Add basic YAML parsing snippet
    This doesn't parse anything meaninful yet, but it will. Trust.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
-fn main() {
+use std::fs::File;
+
+use serde::Deserialize;
+
+#[derive(Debug, Deserialize)]
+struct Blah {
+    foo: i32,
+    bar: i32,
+}
+
+#[derive(Debug, Deserialize)]
+struct SimpleYAML {
+    doe: String,
+    french_hens: i32,
+    calling_birds: Vec<String>,
+    blah: Blah,
+}
+
+fn main() -> anyhow::Result<()> {
     println!("Hello, world!");
+    let f = File::open("./rsrc/simple.yaml")?;
+    let yaml_s: SimpleYAML = serde_yaml::from_reader(f)?;
+    println!("{:#?}", yaml_s);
+
+    return Ok(());
 }
