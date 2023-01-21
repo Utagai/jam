bb352a8a1829ec751cf91ef925ea7135f5dc2eb2
may h <mehrabhoque@gmail.com>
2023-01-21 12:40:22 -0500

    Start deserializing initial form of the config

--- src/main.rs
+++ src/main.rs
@@ -3,23 +3,34 @@ use std::fs::File;
 use serde::Deserialize;
 
 #[derive(Debug, Deserialize)]
-struct Blah {
-    foo: i32,
-    bar: i32,
+struct Target {
+    name: String,
+    // TODO: I believe, if we define custom serialization for this
+    // field, we can make this non-Optional as long as we know
+    // name. There may be conflicts, but those can be reconciled at a
+    // time after serialization.  Plus, the reconciliation logic
+    // likely gets simpler if it can work with String rather than
+    // Option<String>.
+    shortname: Option<String>,
+    help: Option<String>,
+    cmd: Option<String>,
+    targets: Option<Vec<Target>>,
 }
 
 #[derive(Debug, Deserialize)]
-struct SimpleYAML {
-    doe: String,
-    french_hens: i32,
-    calling_birds: Vec<String>,
-    blah: Blah,
+struct Options {}
+
+#[derive(Debug, Deserialize)]
+struct Config {
+    #[serde(flatten)]
+    options: Options,
+    targets: Vec<Target>,
 }
 
 fn main() -> anyhow::Result<()> {
     println!("Hello, world!");
     let f = File::open("./rsrc/simple.yaml")?;
-    let yaml_s: SimpleYAML = serde_yaml::from_reader(f)?;
+    let yaml_s: Config = serde_yaml::from_reader(f)?;
     println!("{:#?}", yaml_s);
 
     return Ok(());
