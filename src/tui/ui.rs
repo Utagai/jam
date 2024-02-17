cd6a3897d184986c9ade3a3b10c8d7459204fba8
may h <mehrabhoque@gmail.com>
2024-02-17 17:47:57 -0500

    Simplify and thin the UI for readability

--- src/tui/ui.rs
+++ src/tui/ui.rs
@@ -1,11 +1,10 @@
 use std::time::{SystemTime, UNIX_EPOCH};
 
 use ratatui::{
-    backend::Backend,
     layout::{Alignment, Constraint, Direction, Layout, Rect},
     style::{Color, Modifier, Style},
     text::{Line, Span},
-    widgets::{Block, BorderType, Borders, Paragraph},
+    widgets::Paragraph,
     Frame,
 };
 
@@ -26,43 +25,26 @@ pub fn ui(f: &mut Frame, state: State) {
     let main_regions = Layout::default()
         .direction(Direction::Vertical)
         .margin(1)
-        // Make the error section always 3 pixels so it has enough
-        // space for a single line of error message + 2 for the
-        // border.
-        .constraints(
-            [
-                Constraint::Max(10),   // 'Keys' window.
-                Constraint::Length(3), // Error section.
-                Constraint::Length(1), // Status line.
-            ]
-            .as_ref(),
-        )
+        .constraints([
+            Constraint::Length(state.key_target_pairs.len() as u16 + 1), // 'Keys' window.
+            Constraint::Length(1), // Current prefix indicator.
+            Constraint::Max(2),    // Error section.
+            Constraint::Length(1), // Ellipses
+        ])
         .split(term_region);
 
-    // UI is simple. We have a block (think div or span), then inside
-    // it is a paragraph. The block has some styling like borders and
-    // a title.
-    // The paragraph just has default style and left-alignment and trimed wrapping.
-    // This call below draws it onto the term.
+    // UI is simple. We have the keys, then a state of the current prefix, a
+    // help section that includes any error messages and a mention of the help
+    // key, and finally, a section for an animation indicating that we are
+    // waiting on input.
     draw_keys(f, main_regions[0], state.key_target_pairs);
-    draw_error(f, main_regions[1], &state.errmsg);
-    draw_statusbar(f, main_regions[2], &state.prefix)
+    draw_current_prefix(f, main_regions[1], &state.prefix);
+    draw_help(f, main_regions[2], &state.errmsg);
+    draw_waiting_anim(f, main_regions[3]);
 }
 
 fn draw_keys(f: &mut Frame, region: Rect, key_target_pairs: &Vec<NextKey>) {
-    let keys_para = Paragraph::new(key_text(key_target_pairs))
-        .block(
-            Block::default()
-                .borders(Borders::ALL)
-                .title(Span::styled(
-                    " jam ",
-                    Style::default()
-                        .add_modifier(Modifier::BOLD)
-                        .fg(Color::LightGreen),
-                ))
-                .border_type(BorderType::Rounded),
-        )
-        .alignment(Alignment::Center);
+    let keys_para = Paragraph::new(key_text(key_target_pairs)).alignment(Alignment::Left);
     f.render_widget(keys_para, region)
 }
 
@@ -77,111 +59,88 @@ fn key_text<'a>(key_to_name: &'a Vec<NextKey>) -> Vec<Line<'a>> {
         })
         .collect();
 
-    let max_target_string_len = std::cmp::max(
-        lines_to_render
-            .iter()
-            .map(|(_, ts, _)| ts.len())
-            .max()
-            .unwrap_or(0),
-        PREFIX_MARKER.len(),
-    );
-
     // Text to show in paragraph.
     let spans_to_render = lines_to_render
         .iter()
-        .map(|(k, target_string, connector)| {
-            generate_spans_for_key(k, target_string, connector, max_target_string_len)
-        })
+        .map(|(k, target_string, connector)| generate_spans_for_key(k, target_string, connector))
         .collect::<Vec<Line>>();
 
     spans_to_render
 }
 
-fn generate_spans_for_key<'a>(
-    k: &'a char,
-    target_string: &'a str,
-    connector: &'a str,
-    max_target_string_len: usize,
-) -> Line<'a> {
-    // So, we are using center alignment. The downside to this is that
-    // the target keys are not all lined up when they are rendered. So
-    // what we really want is to center the text and then align them
-    // on the left. Unfortunately, tui-rs does not support anything
-    // more complex than left/right/center alignment. Therefore, we
-    // solve it ourselves by padding the strings we write with spaces
-    // such that each line is the same length and therefore will be
-    // left justified, while still being rendered in the center of the
-    // screen-width paragraph.
-    let padding = " ".repeat(max_target_string_len - target_string.len());
+fn generate_spans_for_key<'a>(k: &'a char, target_string: &'a str, connector: &'a str) -> Line<'a> {
     Line::from(vec![
-        // ----
         // Key.
         Span::styled(
             format!("{k}"),
             Style::default().add_modifier(Modifier::BOLD),
         ),
-        // --------------------------
         // Connector (e.g. an arrow).
         Span::styled(connector.to_string(), Style::default().fg(Color::DarkGray)),
-        // --------------------------
         // The target name or marker.
         if target_string == PREFIX_MARKER {
             Span::styled(
-                // NOTE: The reason we have a space after the
-                // PREFIX_MARKER in the following string is
-                // pretty subtle and took me a while to
-                // understand. So, what you immediately might
-                // notice is that the difference between the
-                // PREFIX_MARKER and the target names is that
-                // the target names are quoted in the rendered
-                // TUI. So you'd expect, if anything, there to
-                // be a missing _two_ spaces for the _two_
-                // quotes. However, note that we are _center_
-                // aligned. Therefore, a difference of n
-                // characters is going to be chopped in
-                // half. In this case, that means we are off
-                // by 1 space. Try adding another pair of
-                // quotes (') around the target name and
-                // removing the extra space we're putting in
-                // here. You will see that we will now be off
-                // by 4/2 = 2 spaces.
-                format!("{target_string} {padding}"),
+                format!("{target_string}"),
                 Style::default().fg(Color::DarkGray),
             )
         } else {
             Span::styled(
-                format!("'{target_string}'{padding}"),
+                format!("'{target_string}'"),
                 Style::default().fg(Color::LightGreen),
             )
         },
     ])
 }
 
+fn draw_help(f: &mut Frame, region: Rect, errmsg: &str) {
+    if errmsg.is_empty() {
+        draw_help_text(f, region);
+    } else {
+        let subregions = Layout::default()
+            .direction(Direction::Vertical)
+            .constraints([Constraint::Length(1), Constraint::Length(1)])
+            .split(region);
+        draw_error(f, subregions[0], errmsg);
+        draw_help_text(f, subregions[1]);
+    }
+}
+
 fn draw_error(f: &mut Frame, region: Rect, errmsg: &str) {
-    let error_para = Paragraph::new(Line::from(errmsg.to_string())).block(
-        Block::default()
-            .borders(Borders::ALL)
-            .title(Span::styled(
-                " errors ",
-                Style::default()
-                    .add_modifier(Modifier::BOLD)
-                    .fg(Color::LightRed),
-            ))
-            .border_type(BorderType::Rounded),
+    let error_para = Paragraph::new(Line::from(errmsg.to_string())).style(
+        Style::default()
+            .add_modifier(Modifier::BOLD)
+            .fg(Color::LightRed),
     );
     f.render_widget(error_para, region)
 }
 
-fn draw_statusbar(f: &mut Frame, region: Rect, prefix: &Shortcut) {
+fn draw_help_text(f: &mut Frame, region: Rect) {
+    let fg_color_style = Style::default()
+        .fg(Color::DarkGray)
+        .add_modifier(Modifier::ITALIC);
+    let help_text = Paragraph::new("? - help").style(fg_color_style);
+    f.render_widget(help_text, region)
+}
+
+fn draw_current_prefix(f: &mut Frame, region: Rect, prefix: &Shortcut) {
+    let prefix_para = Paragraph::new(Line::from(format!("prefix: '{}'", prefix,))).style(
+        Style::default()
+            .fg(Color::DarkGray)
+            .add_modifier(Modifier::ITALIC),
+    );
+    f.render_widget(prefix_para, region)
+}
+
+fn draw_waiting_anim(f: &mut Frame, region: Rect) {
     let max_num_ellipses: u64 = 3;
     // Divide the given region into the 3 sections of the status bar.
     let status_bar_regions = Layout::default()
-        .direction(Direction::Horizontal)
+        .direction(Direction::Vertical)
         .constraints(
             [
-                Constraint::Percentage(33),
-                Constraint::Percentage(33),
-                Constraint::Percentage(33),
+                Constraint::Length(1),
+                Constraint::Length(1),
+                Constraint::Length(1),
             ]
             .as_ref(),
         )
@@ -197,21 +156,8 @@ fn draw_statusbar(f: &mut Frame, region: Rect, prefix: &Shortcut) {
         .expect("time went backwards")
         .as_secs()
         % (max_num_ellipses + 1);
-    let ellipses = Paragraph::new("•".repeat(num_ellipses as usize))
-        .alignment(Alignment::Center)
-        .style(Style::default());
-
-    // Draw the ends of the sidebar; the prefix tracker and help text.
-    let fg_color_style = Style::default()
-        .fg(Color::DarkGray)
-        .add_modifier(Modifier::ITALIC);
-    let prefix = Paragraph::new(format!("prefix: '{}'", prefix,)).style(fg_color_style);
-    let helptext = Paragraph::new("? - help")
-        .alignment(Alignment::Right)
-        .style(fg_color_style);
+    let ellipses = Paragraph::new("•".repeat(num_ellipses as usize)).style(Style::default());
 
     // Draw the three sections of the status bar:
-    f.render_widget(prefix, status_bar_regions[0]);
     f.render_widget(ellipses, status_bar_regions[1]);
-    f.render_widget(helptext, status_bar_regions[2]);
 }
